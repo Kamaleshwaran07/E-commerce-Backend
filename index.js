f80d491dd1e4a64175fb32ad9f2bb87487c9df63
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2024-10-10 18:42:33 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import app from './app.js'
+
+
+
+dotenv.config()
+
+const mongoDB = process.env.mongoDBConnectionString;
+
+const port = process.env.Port
+
+
+const connectDB = async () => {
+    try {
+        console.log("Connecting to mongoDB")
+        const connection = await mongoose.connect(mongoDB)
+        console.log("Connection Successful");
+        app.listen(port, () => {
+            console.log("App is running in Port =", port);
+        })
+        return connection;
+        
+
+    }
+    catch (error) {
+        console.log("Error connecting to the database", error.message);
+    }
+}
+connectDB()
