b0340783edb4271c611baa21c865d1ee6501b0df
Kamaleshwaran P S <pskamaleshwaran@gmail.com>
2024-11-19 09:25:33 +0530

    auth configured, get user added

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,18 +1,26 @@
 import jwt from 'jsonwebtoken'
+import User from "../Models/usermodel.min.js"
+import dotenv from 'dotenv'
 
-
+dotenv.config()
 const authToken ={
  verifyToken: async (req, res, next)=> {
  try {
     const token = req.cookies.token
-    console.log(token);
+    console.log("Token from auth", token);
     if (!token) {
         res.status(400).json({message:"Token is missing"})
 
     }
     try {
-        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+        const decoded = jwt.verify(token, process.env.JWT_secret)
         req.userId = decoded.id
+        console.log("userId",req.userId)
+        const findUser = await User.findById(decoded.id)
+            if(!findUser){
+                return res.status(404).json({message:"User not found"})
+            }
+            res.status(200).json({message:"User found", findUser})
        next() 
     } catch (error) {
         res.status(401).json({message:"Invalid Token"})
--- Routes/userrouter.js
+++ Routes/userrouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import userController from '../Controllers/usercontroller.min.js'
+import authToken from '../Middleware/auth.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/signup', userController.signup)
 userRouter.post('/login', userController.login)
+userRouter.get('/getuser', authToken.verifyToken, userController.getUserDetails)
 
 
 
