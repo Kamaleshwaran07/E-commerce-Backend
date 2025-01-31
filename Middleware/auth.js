5452b9071a96b67e8242f2fe073ccf6a038db77b
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2025-01-31 15:52:33 +0530

    minor change

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -6,7 +6,7 @@ const authToken = {
   verifyToken: async (req, res, next) => {
     try {
       const token = req.cookies.token;
-      // console.log("Token from auth", token);
+      console.log("Token from auth", token);
       if (!token) {
         return res.status(400).json({ message: "Token is missing" });
       }
