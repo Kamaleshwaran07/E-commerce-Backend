7fa3e1a5ff839c13c896a4bfcb79cdc90bed5325
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2025-02-03 20:36:54 +0530

    console.log added

--- Controllers/usercontroller.js
+++ Controllers/usercontroller.js
@@ -85,6 +85,7 @@ const userController = {
     getAllUsers:async(req, res)=>{
         try {
             const userId = req.userId
+            console.log(userId)
             const user = await User.findById(userId)
             if(!user){
                 return res.status(404).json({message:"User not found"})
